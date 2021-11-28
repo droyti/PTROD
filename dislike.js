3e66746385d3bebf6a48a26ad686d35e431b7b65
Violet <66211311+droyti@users.noreply.github.com>
2021-11-27 19:43:56 -0500

    first basic functional version

--- dislike.js
+++ dislike.js
@@ -1,6 +1,29 @@
-function dislike()
-{
-
+function waitForInfo(el) {
+    return new Promise((resolve, reject) => {
+      const intervalId = setInterval(() => {
+        if (document.evaluate(el, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue) {
+          clearInterval(intervalId);
+          resolve();
+        }
+      }, 500);
+    });
 }
 
-dislike();
+function dislike()
+{    
+    waitForInfo("//yt-formatted-string[text()='Dislike']").then(() => {
+        var xpath = "//yt-formatted-string[text()='Dislike']";
+        var matchingElement = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+        var dislikeButton = matchingElement.parentNode.parentNode;
+    
+        if(!dislikeButton.classList.contains("style-default-active"))
+            dislikeButton.click();
+      });
+}
+
+document.body.addEventListener("yt-navigate-finish", function(event) {
+    dislike();
+});
+
+window.onload = dislike();
+
